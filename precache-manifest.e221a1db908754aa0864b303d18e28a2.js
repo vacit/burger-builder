936edbd100c7c3eb69f8998ba10ec67055015afa
vacit <wajdabdulrazak@gmail.com>
2019-12-07 15:17:24 +0300

    Updates

---
+++ precache-manifest.e221a1db908754aa0864b303d18e28a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "976cf72319f5e7c10b422086c68ef4c0",
+    "url": "/burger-builder/index.html"
+  },
+  {
+    "revision": "9c231de4f9c9fa2eb472",
+    "url": "/burger-builder/static/css/main.d33732c5.chunk.css"
+  },
+  {
+    "revision": "0b7a3f059b71a8b5346c",
+    "url": "/burger-builder/static/js/2.3bd637d7.chunk.js"
+  },
+  {
+    "revision": "9c231de4f9c9fa2eb472",
+    "url": "/burger-builder/static/js/main.faf9f867.chunk.js"
+  },
+  {
+    "revision": "01f389611d85dd339f28",
+    "url": "/burger-builder/static/js/runtime-main.fff5d8f9.js"
+  }
+]);
